63eb34637fdc587adb2832fd272311cfc24fbbd8
Antonin Huard-Lefebvre <antoninhuard.lefebvre@gmail.com>
2024-12-05 21:42:32 -0500

    ajout day2 part 1

--- src/day2/main.go
+++ src/day2/main.go
@@ -1,35 +1,91 @@
 package main
 
 import (
-    "fmt"
-    "aoc-24/src/utils"
+	"aoc-24/src/utils"
+	"bufio"
+	"fmt"
     "strings"
-    "bufio"
-    // "strconv"
+	// "fmt"
+	"os"
+	// "strings"
+	// "bufio"
+	"strconv"
 )
 
-type Order int
 
-type Reports struct {
-    levels []int
-    order Order
-}
+type Order int
 
 const (
     Ascending Order = iota
     Decreasing
 )
 
-func parse(data string)  {
-    lists := strings.Split(data, "\n")
-    fmt.Println(lists)
-    // for _, list := range lists {
-    //     if utils.Abs()
-    // }
+type Report struct {
+    levels []int
+    order Order
+}
 
+func order(levels []int) Order {
+    if levels[0] > levels[len(levels) - 1] {
+        return Decreasing
+    }
+    return Ascending
+}
+
+func isSafe(report Report) bool {
+    if report.order == Ascending {
+        for i := 1; i < len(report.levels); i++ {
+            gap := report.levels[i] - report.levels[i - 1]
+            if gap > 3 || gap < 1 {
+                return false
+            }
+        }
+    } else {
+        for i := 0; i < len(report.levels) - 1; i++ {
+            gap := report.levels[i] - report.levels[i + 1]
+            if gap > 3 || gap < 1 {
+                return false
+            }
+        }
+    }
+    return true 
+}
+
+func solvePartOne(reports []Report) int {
+    count := 0
+    for _, report := range reports {
+        if isSafe(report) {
+            count++
+        }
+    }
+    return count
+}
+
+func parse(file *os.File) []Report {
+    var reports []Report
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        line := strings.TrimSpace(scanner.Text())
+        if line == "" {
+            return reports
+        }
+        nums := strings.Fields(line)
+        var levels []int
+        for _, n := range nums {
+            level, err := strconv.Atoi(n)
+            utils.Check(err) 
+            levels = append(levels, level)
+        }
+        reports = append(reports, Report{levels, order(levels)})
+    }
+    return reports
 }
 
 func main() {
-    data := utils.ReadFile("example.txt")
-    parse(data)
+    file, err := os.Open("input.txt")
+    utils.Check(err)
+    defer file.Close()
+    reports := parse(file)
+    numOfSafeReports := solvePartOne(reports)
+    fmt.Println(numOfSafeReports)
 }
